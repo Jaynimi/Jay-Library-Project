8a1941af79a6b52268f3619d717e50bb848f8ec6
Jaynimi <Eyeslikejays@yahoo.com>
2023-09-01 14:34:21 +0200

    Read Status

--- scrupts.js
+++ scrupts.js
@@ -6,22 +6,24 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const read = document.getElementById("read");
 const form = document.getElementById("form");
+const statuss = document.getElementById("status");
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, statuss) {
 	// the constructor...
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
+	this.statuss = statuss;
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, pages, read, statuss) {
 	// do stuff here
 	if (title.length === 0 || author.length === 0 || pages.length === 0) {
 		alert("Fill all the fields");
 		return;
 	}
-	const newBook = new Book(title, author, pages, read);
+	const newBook = new Book(title, author, pages, read, statuss);
 	// newBook.display();
 	myLibrary.push(newBook);
 	return newBook;
@@ -32,6 +34,7 @@ function displayBooks() {
 	bookSection.innerHTML = myLibrary
 		.map((myBook, index) => {
 			return `<div> Title: ${myBook.title} , Author: ${myBook.author} , Number of Pages: ${myBook.pages} , Pages read: ${myBook.read}
+			<span id="statusDisplay"> Status: ${myBook.statuss} </span>
         <button type="button" class="removeBook" data-index="${index}">Remove</button>
         </div>`;
 		})
@@ -41,6 +44,17 @@ function displayBooks() {
 	const removeButtons = document.querySelectorAll(".removeBook");
 	removeButtons.forEach((button) => {
 		button.addEventListener("click", handleRemoveClick);
+	});
+
+	// Attach event listeners to status display
+	const statusDisplays = document.querySelectorAll(".statusDisplay");
+	statusDisplays.forEach((statusDisplay, index) => {
+		statusDisplay.addEventListener("click", () => {
+			// Toggle the status
+			myLibrary[index].statuss =
+				myLibrary[index].statuss === "read" ? "notRead" : "read";
+			displayBooks(); // Update the displayed books
+		});
 	});
 }
 
@@ -58,8 +72,9 @@ form.addEventListener("submit", (e) => {
 	const newAuthor = author.value;
 	const newPages = pages.value;
 	const newRead = read.value;
+	const newStatuss = statuss.value;
 
-	addBookToLibrary(newTitle, newAuthor, newPages, newRead);
+	addBookToLibrary(newTitle, newAuthor, newPages, newRead, newStatuss);
 	console.log(myLibrary);
 	console.log(newAuthor);
 
