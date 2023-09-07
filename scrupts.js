45ff0d0fd18447f0e3a4772fce0e1031761ea4bd
Jaynimi <Eyeslikejays@yahoo.com>
2023-09-07 23:36:27 +0200

    Extra buttons and functionality

--- scrupts.js
+++ scrupts.js
@@ -23,6 +23,9 @@ function addBookToLibrary(title, author, pages, read, statuss) {
 		alert("Fill all the fields");
 		return;
 	}
+
+	if (read > pages) {
+	}
 	const newBook = new Book(title, author, pages, read, statuss);
 	// newBook.display();
 	myLibrary.push(newBook);
@@ -33,10 +36,14 @@ function addBookToLibrary(title, author, pages, read, statuss) {
 function displayBooks() {
 	bookSection.innerHTML = myLibrary
 		.map((myBook, index) => {
-			return `<div> Title: ${myBook.title} , Author: ${myBook.author} , Number of Pages: ${myBook.pages} , Pages read: ${myBook.read}
+			return `<div> Title: ${myBook.title} , Author: ${myBook.author} , Number of Pages: ${myBook.pages} ,
+			<span> Pages read: <span class="pagesRead">${myBook.read}</span> 
+			<i class="fa-solid fa-arrow-up increaseRead" data-index="${index}"></i>
+			<i class="fa-solid fa-arrow-down decreaseRead" data-index="${index}"></i> 
+			</span>
 			<span id="statusDisplay"> Status: ${myBook.statuss} </span>
-        <button type="button" class="removeBook" data-index="${index}">Remove</button>
-        </div>`;
+			<button type="button" class="removeBook" data-index="${index}">Remove</button>
+		  </div>`;
 		})
 		.join("");
 
@@ -46,13 +53,48 @@ function displayBooks() {
 		button.addEventListener("click", handleRemoveClick);
 	});
 
-	// Attach event listeners to status display
-	const statusDisplays = document.querySelectorAll(".statusDisplay");
+	// Attach event listeners to increaseRead buttons
+	const increaseButtons = document.querySelectorAll(".increaseRead");
+	increaseButtons.forEach((button) => {
+		button.addEventListener("click", (e) => {
+			const index = e.target.getAttribute("data-index");
+			myLibrary[index].read++; // Increase the read count
+			// Update the displayed pages read
+			const pagesReadElement = document.querySelector(`.pagesRead`);
+			pagesReadElement.textContent = myLibrary[index].read;
+			// Call displayBooks to update the display
+			displayBooks();
+		});
+	});
+
+	// Attach event listeners to decreaseRead buttons
+	const decreaseButtons = document.querySelectorAll(".decreaseRead");
+	decreaseButtons.forEach((button) => {
+		button.addEventListener("click", (e) => {
+			const index = e.target.getAttribute("data-index");
+			if (myLibrary[index].read > 0) {
+				myLibrary[index].read--; // Decrease the read count (if greater than 0)
+				// Update the displayed pages read
+				const pagesReadElement = document.querySelector(`.pagesRead`);
+				pagesReadElement.textContent = myLibrary[index].read;
+
+				// Call displayBooks to update the display
+				displayBooks();
+			}
+		});
+	});
+
+	// Attach event listeners to Read Status display
+	const statusDisplays = document.querySelectorAll("#statusDisplay");
 	statusDisplays.forEach((statusDisplay, index) => {
 		statusDisplay.addEventListener("click", () => {
 			// Toggle the status
 			myLibrary[index].statuss =
-				myLibrary[index].statuss === "read" ? "notRead" : "read";
+				myLibrary[index].statuss === "Read" ? "Not Read" : "Read";
+
+			if (myLibrary[index].statuss === "Not Read") {
+				myLibrary[index].read = 0;
+			}
 			displayBooks(); // Update the displayed books
 		});
 	});
